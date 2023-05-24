641ce80a48836f60bef966bd6fabe2d8c56e47b4
eminezubeyde <113181394+eminezubeyde@users.noreply.github.com>
2023-05-25 00:27:48 +0300

    exception structures added

---
+++ bankApp/src/main/java/com/example/bankApp/common/core/exception/AlreadyExistsException.java
@@ -0,0 +1,8 @@
+package com.example.bankApp.common.core.exception;
+
+public class AlreadyExistsException extends GeneralException{
+
+    public AlreadyExistsException(String message) {
+        super(message);
+    }
+}
